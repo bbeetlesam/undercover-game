fe0e413fb49845920a4ba0661dca9846db519c2c
bbeetlesam <samsismines@gmail.com>
2025-03-25 01:32:12 +0700

    feat: relayout main page card, delete unnecessary codes, add input box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,58 @@
 // App (all components are wrapped in StrictMode at main.jsx)
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/icon.svg'
+import { useState } from "react";
 import './App.css'
 
 import Header from './components/Header'
 
 function App() {
-    const [count, setCount] = useState(0)
-
+    const [username, setUsername] = useState("");
+    const [warningMessage, setWarningMessage] = useState("");
+    
+    const handleInputChange = (text) => {
+        let value = text.target.value.replace(/\s/g, ''); // Restrict whitespace
+        
+        if (value.length > 10) {
+            setWarningMessage("Maximum 12 characters");
+        } else if (text.target.value.includes(' ')) {
+            setWarningMessage("Cannot contain a whitespace");
+        } else {
+            setWarningMessage("");
+        }
+        
+        if (value.length <= 10) {
+            setUsername(value);
+        }
+    };
+    
     return (
         <>
             <Header />
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Undercover Game</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
+            <div className="home-container">
+                <h1 className="title">Undercover Who</h1>
+                <div className="username-container">
+                    <input
+                        type="text"
+                        className="username-input"
+                        value={username}
+                        onChange={handleInputChange}
+                        onBlur={() => setWarningMessage("")}
+                        placeholder="Enter your username"
+                    />
+                    {warningMessage && <p className="warning-input">{warningMessage}</p>}
+                </div>
+                <div className="button-card">
+                    <button className="card-button">
+                        <p>Create Room</p>
+                    </button>
+                    <button className="card-button">
+                        <p>Join Room</p>
+                    </button>
+                </div>
+                <p>I'm a highway star!!</p>
+                <p className="read">
+                    by Gus Javar Pasuruan
                 </p>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     )
 }
