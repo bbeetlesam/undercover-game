0ab3e85dc9194827fe084a43308b29cb90586cb3
bbeetlesam <samsismines@gmail.com>
2025-03-27 17:43:40 +0700

    refactor: set view router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
 // App (all components are wrapped in StrictMode at main.jsx)
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 
 import Header from './components/Header'
 import HomeView from './components/HomeView'
+import CreateRoomView from './components/CreateRoomView'
+import JoinRoomView from './components/JoinRoomView'
 
 function App() {
     return (
         <>
             <Header />
-            <HomeView />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomeView />} />
+                    <Route path="/create-room" element={<CreateRoomView />} />
+                    <Route path="/join-room" element={<JoinRoomView />} />
+                </Routes>
+            </Router>
         </>
     )
 }
